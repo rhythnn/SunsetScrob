bcf3d170fd1bf6b36c579bd05b53eb62bb960a08
Takuma Homma <nagomimatcha@gmail.com>
2018-08-14 16:24:41 +0900

    Sample for Layout Performance [skip ci]

--- app/src/main/java/com/mataku/scrobscrob/app/ui/MainActivity.kt
+++ app/src/main/java/com/mataku/scrobscrob/app/ui/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Handler
 import android.provider.Settings
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.FrameMetrics
 import android.view.Menu
 import android.view.MenuItem
+import android.view.Window
 import com.mataku.scrobscrob.R
 import com.mataku.scrobscrob.app.data.Migration
 import com.mataku.scrobscrob.app.receiver.AppleMusicNotificationReceiver
@@ -26,6 +30,45 @@ class MainActivity : AppCompatActivity(), MainViewCallback {
     private lateinit var binding: ActivityMainBinding
 
     private val self = this
+
+    private lateinit var metrics: FrameMetrics
+
+    val runnable = Runnable {
+        Log.d("MATAKUDEBUG Metrics", "ANIMATION_DURATION: " + metrics.getMetric((FrameMetrics.ANIMATION_DURATION)) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "COMMAND_ISSUE_DURATION: " + metrics.getMetric(FrameMetrics.COMMAND_ISSUE_DURATION) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "DRAW_DURATION: " + metrics.getMetric(FrameMetrics.DRAW_DURATION) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "FIRST_DRAW_FRAME: " + metrics.getMetric(FrameMetrics.FIRST_DRAW_FRAME) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "INPUT_HANDLING_DURATION: " + metrics.getMetric(FrameMetrics.INPUT_HANDLING_DURATION) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "LAYOUT_MEASURE_DURATION: " + metrics.getMetric(FrameMetrics.LAYOUT_MEASURE_DURATION) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "SWAP_BUFFERS_DURATION: " + metrics.getMetric(FrameMetrics.SWAP_BUFFERS_DURATION) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "SYNC_DURATION: " + metrics.getMetric(FrameMetrics.SYNC_DURATION) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "TOTAL_DURATION: " + metrics.getMetric(FrameMetrics.TOTAL_DURATION) / Math.pow(10.0, 6.0))
+        Log.d("MATAKUDEBUG Metrics", "UNKNOWN_DELAY_DURATION: " + metrics.getMetric(FrameMetrics.UNKNOWN_DELAY_DURATION) / Math.pow(10.0, 6.0))
+    }
+
+    private val frameMetricsHandler = Handler()
+
+    private val frameMetricsAvailableListener = Window.OnFrameMetricsAvailableListener { _, frameMetrics, _ ->
+        val frameMetricsCopy = FrameMetrics(frameMetrics)
+        // Layout measure duration in Nano seconds
+        val layoutMeasureDurationNs = frameMetricsCopy.getMetric(FrameMetrics.LAYOUT_MEASURE_DURATION)
+
+        Log.d("MATAKUDEBUG", "layoutMeasureDurationNs: $layoutMeasureDurationNs")
+    }
+
+
+    val listener = Window.OnFrameMetricsAvailableListener { window, frameMetrics, dropCountSinceLastInvocation ->
+        Log.i("MATAKUDEBUG", "onFrame")
+        metrics = FrameMetrics(frameMetrics)
+    }
+
+    val handler = Handler()
+
+    override fun onResume() {
+        super.onResume()
+//        window.addOnFrameMetricsAvailableListener(frameMetricsAvailableListener, frameMetricsHandler)
+        window.addOnFrameMetricsAvailableListener(listener, frameMetricsHandler)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,11 +90,14 @@ class MainActivity : AppCompatActivity(), MainViewCallback {
         filter.addAction("AppleMusic")
         registerReceiver(receiver, filter)
         setUpContentTab()
+        handler.postDelayed(runnable, 2000)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(receiver)
+        window.removeOnFrameMetricsAvailableListener(listener)
+//        window.removeOnFrameMetricsAvailableListener(frameMetricsAvailableListener)
     }
 
     //    右上のメニューボタン表示
